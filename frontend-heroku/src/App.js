67542a3ddd008b3853744011ecee5d9db4762156
Sital Sitoula <heavywater221@gmail.com>
2022-02-25 15:49:36 -0600

    update yaml

--- frontend-heroku/src/App.js
+++ frontend-heroku/src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    fetch("https://dashboard.heroku.com/apps/dfh-frontend/movies/list", {
+    fetch("https://serene-scrubland-55953.herokuapp.com/movies/list", {
       method: "GET",
       headers: {
         Accept: "application/json",
