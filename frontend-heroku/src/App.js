1e5ed4711df3c0bd51b1e190e0e45887e31c3777
Sital Sitoula <heavywater221@gmail.com>
2022-02-24 20:06:40 -0600

    basic api

---
+++ frontend-heroku/src/App.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("movies/list", {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.movies);
+        setMovies(data.movies);
+      });
+  }, []);
+
+  return (
+    <div>
+      {movies.map((item, index) => {
+        return (
+      
+            <li key={index}> {item.name}</li>
+         
+        );
+      })}
+    </div>
+  );
+};
+
+export default App;
